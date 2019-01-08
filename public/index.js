0e68186cdc60a09418b72b4794663a8ed8c8a17f
Julian WEBER <julian.weber@hotmail.fr>
2019-01-08 19:00:35 +0100

    First PW

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 /* global PRIVATEASER*/
 'use strict';
 
+
 (() => {
   const render = (actors) => {
     const fragment = document.createDocumentFragment();
@@ -21,13 +22,55 @@
     document.querySelector('#actors').appendChild(fragment);
   };
 
+  
+
   const button = document.querySelector('#compute');
 
   button.addEventListener('click', function onClick () {
     const bar = PRIVATEASER.getBar();
-    const time = document.querySelector('.time').value;
-    const persons = document.querySelector('.persons').value;
-    const option = document.querySelector('.option').checked;
+    const time = document.getElementById('form-time').value;
+    const persons = document.querySelector('form-persons').value;
+    const option = document.querySelector('form-option').checked;
+    const actors = PRIVATEASER.payActors(bar, time, persons, option);
+
+    render(actors);
+
+    return;
+  });
+
+  
+
+  const start = document.querySelector('#start');
+
+  start.addEventListener('click', function onClick () {
+    document.getElementById("jumbotron").style.display = 'none'
+    document.getElementById("stepper").style.display = "initial";
+    console.log("Start")
+    return;
+  });
+
+  const goBooker = document.querySelector('#goBooker');
+
+  goBooker.addEventListener('click', function onClick () {
+    document.getElementById("bar-stepper").className = 'completed'
+    document.getElementById("bar-container").style.display = 'none'
+    document.getElementById("booker-stepper").className = 'active'
+    document.getElementById("booker-container").style.display = 'initial'
+    return;
+  });
+
+  const goResult = document.querySelector('#goResult');
+
+  goResult.addEventListener('click', function onClick () {
+    document.getElementById("booker-stepper").className = 'completed'
+    document.getElementById("booker-container").style.display = 'none'
+    document.getElementById("result-stepper").className = 'active'
+    document.getElementById("result-container").style.display = 'initial'
+
+    const bar = PRIVATEASER.getBar();
+    const time = document.getElementById('form-time').value;
+    const persons = document.querySelector('form-persons').value;
+    const option = document.querySelector('form-option').checked;
     const actors = PRIVATEASER.payActors(bar, time, persons, option);
 
     render(actors);
